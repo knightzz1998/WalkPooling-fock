249e3050a0079aa380e0f22570c155f83405b2e5
王天赐 <1029836827@qq.com>
2022-04-16 10:48:47 +0800

    添加不同版本的依赖下载脚本

---
+++ bash/env_cu113.sh
@@ -0,0 +1,6 @@
+pip install torch==1.10.0+cu113 -f https://download.pytorch.org/whl/torch_stable.html
+# CUDA 11.3
+pip install torch-scatter==2.0.9 -f https://pytorch-geometric.com/whl/torch-1.10.0+cu113.html
+pip install torch-cluster==1.5.9 -f https://pytorch-geometric.com/whl/torch-1.10.0+cu113.html
+pip install torch-spline-conv==1.2.1 -f https://pytorch-geometric.com/whl/torch-1.10.0+cu113.html
+pip install torch-sparse==0.6.12 -f https://pytorch-geometric.com/whl/torch-1.10.0+cu113.html
